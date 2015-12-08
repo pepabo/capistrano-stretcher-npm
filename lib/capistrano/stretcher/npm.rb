77a15568bd0f36648cd1c33c9f408d338baffb60
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-12-08 14:31:35 +0900

    bundle gem capistrano-stretcher-npm

---
+++ lib/capistrano/stretcher/npm.rb
@@ -0,0 +1,9 @@
+require "capistrano/stretcher/npm/version"
+
+module Capistrano
+  module Stretcher
+    module Npm
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano/stretcher/npm/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Stretcher
+    module Npm
+      VERSION = "0.1.0"
+    end
+  end
+end
